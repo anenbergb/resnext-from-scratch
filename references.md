5801a2edc6bb011d140b424155c3508bd7cfa6b7
Bryan Anenberg <anenbergb@gmail.com>
2025-02-05 11:15:05 -0800

    initial check-in

---
+++ references.md
@@ -0,0 +1,44 @@
+# ResNeXt References
+
+Benchmarks
+* https://pytorch.org/vision/main/models.html#classification
+
+Accuracies on ImageNet-1K using single crops:
+
+| Model Name | 
+
+ResNeXt101_64X4D_Weights.IMAGENET1K_V1
+
+83.246
+
+96.454
+
+83.5M
+
+15.46
+
+link
+
+ResNeXt50_32X4D_Weights.IMAGENET1K_V1
+
+77.618
+
+93.698
+
+25.0M
+
+4.23
+
+link
+
+ResNeXt50_32X4D_Weights.IMAGENET1K_V2
+
+81.198
+
+95.34
+
+25.0M
+
+4.23
+
+link
---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="resnext-from-scratch",
+    version="1.0.0",
+    url="https://github.com/anenbergb/resnext-from-scratch",
+    author="Bryan Anenberg",
+    author_email="anenbergb@gmail.com",
+    description="From scratch implentation of ResNeXt image classifier",
+    python_requires=">=3.10",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "tensorboard",
+        "pandas",
+        "pandas-stubs",
+        "opencv-python",
+        "loguru",
+        "matplotlib",
+        "ffmpeg-python",
+        "tqdm",
+        "types-tqdm",
+        "pillow",
+        "types-Pillow",
+        "tabulate",
+        "fiftyone",
+        "huggingface_hub[cli,torch]",
+        "datasets",
+    ],
+    extras_require={
+        "torch": [
+            "torch",
+            "torchvision",
+        ],
+        "notebook": [
+            "jupyter",
+            "itkwidgets",
+            "jupyter_contrib_nbextensions",
+            "plotly",
+            "seaborn",
+            "fiftyone",
+        ],
+        # conda install ipykernel
+        "dev": ["black", "mypy", "flake8", "isort", "ipdb"],
+    },
+)
