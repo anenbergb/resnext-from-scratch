29b8f0b9cb79ebdd95dbadd5219ca456500387b2
Bryan Anenberg <anenbergb@gmail.com>
2025-10-25 07:51:31 -0700

    commit notebooks

--- train_jax.sh
+++ train_jax.sh
@@ -4,8 +4,9 @@ eval "$(conda shell.bash hook)"
 export CUDA_VISIBLE_DEVICES=0
 
 conda activate jax
-# JAX_DISABLE_JIT=1 
+# JAX_DISABLE_JIT=1
 JAX_PLATFORM_NAME=gpu python resnext/train_jax.py \
---output-dir /media/bryan/ssd01/expr/resnext_from_scratch/debug02 \
---batch-size 32 \
---epochs 10 --lr-warmup-epochs 2 --limit-train-iters 100 --limit-val-iters 100
+--output-dir /media/bryan/ssd01/expr/resnext_from_scratch/jax \
+--batch-size 256 \
+--epochs 100 --lr-warmup-epochs 5
+# --limit-train-iters 1000 --limit-val-iters 100
